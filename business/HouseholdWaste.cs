48a5f2c577a1c3fac1a91b9b97f30da09ee6aa9b
Parkavan Illavalagan <86165957+parkavai@users.noreply.github.com>
2023-05-05 20:31:45 +0200

    Added C# Classes

---
+++ business/HouseholdWaste.cs
@@ -0,0 +1,6 @@
+using System;
+using System.IO;
+
+public class HouseholdWaste : Pickup{
+    int kostnad = 100;
+}
---
+++ business/Paper.cs
@@ -0,0 +1,9 @@
+using System;
+using System.IO;
+
+public class Paper : Pickup{
+    
+    Paper(int n){
+        kostnad = n;
+    }
+}
